f530877a6cdf6a40f6a0e0312419f69f82e15ce8
tinocoAlexander <alexnuevo040@gmail.com>
2025-01-26 23:42:46 -0600

    Introduccion a modelos

--- djangoPractica/djangoPractica/settings.py
+++ djangoPractica/djangoPractica/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'moduloOne',
+    'moduloOne.apps.ModulooneConfig',
 ]
 
 MIDDLEWARE = [
