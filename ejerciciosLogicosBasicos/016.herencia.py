a9afde50caafc3e38193de8600466ffbb28ecb39
tinocoAlexander <alexnuevo040@gmail.com>
2025-01-28 23:30:12 -0600

    Herencia e iteradores

--- ejerciciosLogicosBasicos/016.herencia.py
+++ ejerciciosLogicosBasicos/016.herencia.py
@@ -1,1 +1,28 @@
-# Dejamos la plantilla para poder hacer el archivo mañana :)
+class Animal:
+    def __init__(self, nombre, edad, genero):
+        self.nombre = nombre
+        self.edad = edad
+        self.genero = genero
+
+    def __str__(self):
+        return f"Este es un {self.nombre} y tiene la edad de {self.edad} años"
+
+    def comer(self):
+        print("El animal está comiendo")
+
+class Burro(Animal):
+    def __init__(self, nombre, edad, genero, color):
+        super().__init__(nombre, edad, genero)
+        self.color = color
+
+    def __str__(self):
+        return super().__str__() + f" y su color es {self.color}"
+
+    def comer(self):
+        print(super().comer() + f"y su color es {self.color}")
+
+animal1 = Animal("animalito",10,"Masculino")
+print(animal1)
+
+burro1 = Burro("Burrito", 2, "Femenino", "Cafe")
+print(burro1)
---
+++ ejerciciosLogicosBasicos/017.iteradores.py
@@ -0,0 +1,10 @@
+# En python existen cosas que son iteradores
+
+# Estos cumplen la misma funcion que la misma funcion que un for, iterar pues una lista o algo asi
+
+miListaMercado = ["manzanas", "platanos", "aguacates"]
+iterarLista = iter(miListaMercado)
+
+print(next(iterarLista))  # "manzanas"
+print(next(iterarLista))  # "platanos"
+print(next(iterarLista))  # "aguacates"
